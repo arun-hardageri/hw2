d0a9d0fd65dde0bd8c55c18553619c1a10ddc118
arun-hardageri <arun.hardageri@kellogg.northwestern.edu>
2018-02-12 21:22:06 -0600

    Minor wording update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let imageSwitch = function ()
   let theImage = jQuery(event.target)
   // Capture the source path of the thumbnail image that was clicked
   let srcValue = theImage.attr("src")
-  // Switch the source of the main image with the source captured from the clicked thumbnail
+  // Switch the source path of the main image with the source path captured from the clicked thumbnail
   jQuery("#main-image").attr("src",srcValue)
 }
 jQuery ("#thumbnails").on("click",imageSwitch)
