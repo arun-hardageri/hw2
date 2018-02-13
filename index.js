54f44c5d2493d103804caf17629dca8638bad396
arun-hardageri <arun.hardageri@kellogg.northwestern.edu>
2018-02-12 21:20:07 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let imageSwitch = function ()
+{
+  let theImage = jQuery(event.target)
+  // Capture the source path of the thumbnail image that was clicked
+  let srcValue = theImage.attr("src")
+  // Switch the source of the main image with the source captured from the clicked thumbnail
+  jQuery("#main-image").attr("src",srcValue)
+}
+jQuery ("#thumbnails").on("click",imageSwitch)
